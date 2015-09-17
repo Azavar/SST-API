d9350126d392095c0bf053e8fd35b24f23bd9fb2
azavar <ssnively@azavar.com>
2015-09-17 15:01:25 -0500

    Sample Code

---
+++ Web Service Sample Code/WSClient/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WSClient.ServiceReference1;
+
+namespace WSClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Run a test as a State
+            State.Test();
+
+            //Run a test Registration as a CSP
+            //Registration.Test();
+            
+
+            Console.WriteLine("Press any key to continue");
+            Console.ReadKey();
+        }
+
+    }
+}
